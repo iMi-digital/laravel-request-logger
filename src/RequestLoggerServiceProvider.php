1bc5524cdc8935f40a6da5c8ef9ebd639ec59607
Agel_Nash <agel-nash@yandex.com>
2019-05-26 00:44:29 +0500

    feat: extend method field
    The DELETE method doesn't supported

--- src/RequestLoggerServiceProvider.php
+++ src/RequestLoggerServiceProvider.php
@@ -9,8 +9,7 @@ class RequestLoggerServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $filename = __DIR__ . '/../database/migrations/2015_09_25_111650_create_request_log_entries_table.php';
-        $this->publishes([$filename => database_path('migrations/2015_09_25_111650_create_request_log_entries_table.php')], 'migrations');
+        $this->publishes([__DIR__ . '/../database/migrations/' => database_path('migrations/')], 'migrations');
     }
 
     public function register()
